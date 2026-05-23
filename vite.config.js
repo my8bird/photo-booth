c5e220d0c9a29dbc8e71a0f47cb88c11b11ef8c9
Nathan Landis <nlandis@veracode.com>
2026-05-23 14:19:42 -0500

    Update prefix to see if GH will allow JS downloads

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dean/',
+  base: '/photo-booth/',
   server: {
     open: true,
     port: 5173
